f8640cd888d8fa43433853513d843522be2daaaa
Matthew Zhang <mzhang1403@inst.hcpss.org>
2019-04-01 09:03:12 -0400

    stuff

--- src/WorldBuilder.java
+++ src/WorldBuilder.java
@@ -1,4 +1,15 @@
 
 public class WorldBuilder {
-
+	final int WORLDHEIGHT = 50;
+	final int BASEDIRTHEIGHT = 12;
+	private void genSnow(int width, int startHeight, int endHeight) {
+		Block[][] world = new Block[WORLDHEIGHT][width];
+		for(int i=0; i<width; i++) {
+			int dirtHeight = BASEDIRTHEIGHT+(int)(3d*Math.random()-1);
+			for(int j=0;j<BASEDIRTHEIGHT; j++) {
+				world[j][i] = new Block(Block.DIRT);
+			}
+		}
+		return world;
+	}
 }
