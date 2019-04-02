62f11740683ddd0fdcd3962e0af37042633c43e9
Helen Yang <hyang6036@inst.hcpss.org>
2019-04-02 09:10:53 -0400

    aaaaa

--- src/BlockEnum.java
+++ src/BlockEnum.java
@@ -1,20 +1,46 @@
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
 
 public enum BlockEnum {
-	DIRT (),
-	GRASS (),
-	SAND (),
-	SNOW (),
-	TREETRUNK (),
-	TREELEAF (),
-	BRICK (),
-	ROOF1 (),
-	ROOF2 (),
-	ROCK (),
-	WATER ();
+	DIRT ("dirt"),
+	/*GRASS ("Pictures/grass.png"),
+	SAND ("Pictures/sand.png"),
+	SNOW ("Pictures/snow.png"),
+	TREETRUNK ("Pictures/treetrunk.png"),
+	TREELEAF ("Pictures/treeleaf.png"),
+	BRICK ("Pictures/brick.png"),
+	ROOF1 ("Pictures/roof1.png"),
+	ROOF2 ("Pictures/roof2.png"),
+	ROCK ("Pictures/rock.png"),
+	WATER ("Pictures/water.png")*/;
 	
-	private int type;
-	private BufferedImage image;
+	private static final String pathStart = "Pictures/";
+	private static final String pathEnd = ".png";
+	
+	private final BufferedImage image;
+	private final char type = 'X';
+	
+	private BlockEnum(String name) {
+		BufferedImage tempImage = null;
+		try {
+			tempImage = ImageIO.read(new File(fileName));
+		} catch (Exception e) {
+			tempImage = null;
+			System.out.println("NULL BLOCK IMAGE");
+		}
+		image = tempImage;
+		type = name.charAt(0);
+	}
+	
+	public BufferedImage getImage() {
+		return image;
+	}
+	
+	public String toString() {
+		return "
+	}
 	
 }
--- src/Screen.java
+++ src/Screen.java
@@ -1,14 +1,30 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
-public class Screen extends JPanel {
+public class Screen extends JPanel implements KeyListener {
 
 	private JFrame frame;
+	private int xPos = 0;
+	private int yPos = 0;
+	private BufferedImage charImage;
 	
 	public Screen() {
 		initializeSwing();
+		BufferedImage tempImage = null;
+		try {
+			tempImage = ImageIO.read(new File("Pictures/guy.png"));
+		} catch (Exception e) {
+			tempImage = null;
+			System.out.println("NULL CHAR IMAGE");
+		}
+		charImage = tempImage;
 	}
 	
 	private void initializeSwing() {
@@ -17,6 +33,7 @@ public class Screen extends JPanel {
 		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		//frame.setUndecorated(true);
 		frame.setVisible(true);
+		frame.addKeyListener(this);
 	}
 	
 	public static void main(String[] args) {
@@ -24,12 +41,59 @@ public class Screen extends JPanel {
 	}
 	
 	public void paintComponent(Graphics g) {
-		g.setColor(Color.BLACK);
-		g.fillRect(100, 100, 50, 50);
+		super.paintComponent(g);
+		BlockEnum[][] worldTest = new BlockEnum[5][3];
+		for (int r = 0; r < worldTest.length; r++) {
+			for (int c = 0; c < worldTest[0].length; c++) {
+				worldTest[r][c] = BlockEnum.DIRT;
+			}
+		}
+		drawWorld(g, worldTest);
+	}
+	
+	private void drawWorld(Graphics g, BlockEnum[][] world) {
+		for (int r = 0; r < world.length; r++) {
+			for (int c = 0; c < world[0].length; c++) {
+				drawBlock(g, world[r][c], (c*50) + xPos, (r*50) + yPos);
+			}
+		}
+	}
+	
+	private void drawChar() {
+		g.drawImage()
+	}
+	private void drawBlock(Graphics g, BlockEnum b, int x, int y) {
+		g.drawImage(b.getImage(), x, y, this);
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		//System.out.println(e.getKeyCode());
+		int increment = 5;
+		//right
+		if (e.getKeyCode() == 39) {
+			xPos -= increment;
+		}
+		//up 
+		else if (e.getKeyCode() == 38) {
+			yPos += increment;
+		}
+		//left
+		else if (e.getKeyCode() == 37) {
+			xPos += increment;
+		}
+		repaint();
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
 		
 	}
-	
-	private void drawBlock(Graphics g, Block b) {
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
-	
 }
