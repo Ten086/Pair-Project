b0bf99d2849677f8429b671bad15168ee41802c3
Matthew Zhang <mzhang1403@inst.hcpss.org>
2019-04-08 08:32:50 -0400

    stuff

--- src/Screen.java
+++ src/Screen.java
@@ -1,7 +1,9 @@
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -16,37 +18,31 @@ import javax.swing.*;
 
 public class Screen extends JPanel implements KeyListener, ActionListener {
 
-	private boolean gameRunning = true;
-	private Timer timer;
 	private JFrame frame;
+	private Timer timer;
 	private BlockEnum[][] world;
-	public static final int BLOCKSIZE = 30;
-	private int charX = 600;
-	private int charY = 300;
-	private int xPos = 600;
-	private int yPos = 1800;
+	private static final int SCREENWIDTH = Toolkit.getDefaultToolkit().getScreenSize().width;
+	private static final int SCREENHEIGHT = Toolkit.getDefaultToolkit().getScreenSize().height;
+	private static final int BLOCKSWIDE = 100;
+	public static final int BLOCKSIZE = SCREENWIDTH / BLOCKSWIDE;
+	public static final int MOVEINCREMENT = (BLOCKSIZE / 4) * 3;
+	private final int charX = BLOCKSIZE * (BLOCKSWIDE / 2);
+	private final int charY = SCREENHEIGHT / 2 - BLOCKSIZE;
+	private int xPos = -43000;
+	private int yPos = BLOCKSIZE * 80;
 	private Character character;
 	private Rectangle charRect = new Rectangle(charX, charY, BLOCKSIZE, BLOCKSIZE * 2);
-	private Rectangle belowRect;
-	private Rectangle leftRect;
-	private Rectangle rightRect;
-	ArrayList<Integer> keysPressed = new ArrayList<Integer>();
 	private int xVel = 0;
 	private int yVel = 0;
-	public static final int MOVEINCREMENT = 10;
+	private Rectangle belowRect;
+	ArrayList<Integer> keysPressed = new ArrayList<Integer>();
 
 	public Screen() {
 		character = new Character("character");
-
-		//System.out.println("before gen");
 		world = WorldBuilder.genWorld();
-		//System.out.println("after gen");
+		System.out.println(BLOCKSIZE);
 		initializeSwing();
-
-		touchBelow();
-
-		timer = new Timer(5, this);
-		timer.setInitialDelay(100);
+		timer = new Timer(10, this);
 		timer.start();
 	}
 
@@ -64,89 +60,50 @@ public class Screen extends JPanel implements KeyListener, ActionListener {
 		frame.addKeyListener(this);
 	}
 
-	private int[] getBlockBelow() {
-		int row = -1;
-		int col = -1;
-		for (int c = 0; c < world[0].length; c++) {
-			int leftPixel = c * BLOCKSIZE + xPos;
-			int rightPixel = leftPixel + BLOCKSIZE;
-			if (leftPixel <= charX && rightPixel > charX) {
-				col = c;
-			}
-		}
-		//System.out.println("col: " + col);
-		for (int r = 0; r < world.length; r++) {
-			if (world[r][col] != null) {
-				row = r;
-			}
-		}
-		//System.out.println("row: " + row);
-		return new int[] {row, col};
+	private int rowToY(int blockRow) {
+		return yPos - (blockRow * BLOCKSIZE);
 	}
 
-	private int[] getBlockLeft() {
-		int row = -1;
-		int col = -1;
-		for (int c = 0; c < world[0].length; c++) {
-			int rightPixel = c * BLOCKSIZE + xPos + BLOCKSIZE;
-			if (rightPixel < charX) {
-				col = c;
-			}
-		}
-		for (int r = 0; r < world.length; r++) {
-			int topPixel = yPos - (r * BLOCKSIZE);
-			if (topPixel > charY && topPixel < charY + (2 * BLOCKSIZE)) {
-				row = r;
-			}
-		}
-		return new int[] {row, col};
+	private int colToX(int blockCol) {
+		return xPos + (blockCol * BLOCKSIZE);
+	}
+	private Rectangle makeRectangle(int blockRow, int blockCol) {
+		return new Rectangle(colToX(blockCol), rowToY(blockRow), BLOCKSIZE, BLOCKSIZE);
 	}
 
-	private int[] getBlockRight() {
-		int row = -1;
-		int col = -1;
-		for (int c = world[0].length - 1; c >= 0; c--) {
-			int leftPixel = c * BLOCKSIZE + xPos;
-			if (leftPixel > charX) {
-				col = c;
-			}
+	private int[] belowRowCol() {
+		int col = xToCol(charX);
+		int row = yToRow(world.length - 1);
+		while (world[row][col] == null || world[row][col] == BlockEnum.WATER) {
+			row--;
 		}
-		for (int r = 0; r < world.length; r++) {
-			int topPixel = yPos - (r * BLOCKSIZE);
-			if (topPixel > charY && topPixel < charY + (2 * BLOCKSIZE)) {
-				row = r;
-			}
-		}
-		return new int[] {row, col};
-	}
-	private boolean touchLeft() {
-		int blockRow = getBlockLeft()[0];
-		int blockCol = getBlockLeft()[1];
-		if (world[blockRow][blockCol] == null) {
-			return false;
-		}
-		else {
-			leftRect = new Rectangle(xPos + (blockCol * BLOCKSIZE), yPos - (blockRow * BLOCKSIZE), BLOCKSIZE, BLOCKSIZE);
-			return charRect.intersects(leftRect);
-		}
+		int[] coords = {row, col};
+		return coords;
 	}
 
-	private boolean touchRight() {
-		int blockRow = getBlockRight()[0];
-		int blockCol = getBlockRight()[1];
-		if (world[blockRow][blockCol] == null) {
-			return false;
-		}
-		else {
-			rightRect = new Rectangle(xPos + (blockCol * BLOCKSIZE), yPos - (blockRow * BLOCKSIZE), BLOCKSIZE, BLOCKSIZE);
-			return charRect.intersects(rightRect);
-		}
+	private int belowRow() {
+		return belowRowCol()[0];
 	}
-	private boolean touchBelow() {
-		int blockRow = getBlockBelow()[0];
-		int blockCol = getBlockBelow()[1];
-		belowRect = new Rectangle(xPos + (blockCol * BLOCKSIZE), yPos - (blockRow * BLOCKSIZE), BLOCKSIZE, BLOCKSIZE);
-		return charRect.intersects(belowRect);
+
+	private int belowCol() {
+		return belowRowCol()[1];
+	}
+
+	private int belowY() {
+		return rowToY(belowRow());
+	}
+
+	private int belowX() {
+		return colToX(belowCol());
+	}
+
+	private int distanceBelow() {
+		return ((charY + (BLOCKSIZE * 2) - belowY()));
+	}
+
+	private boolean collidesBelow() {
+		belowRect = makeRectangle(belowRow(), belowCol());
+		return belowRect.intersects(charRect);
 	}
 
 	public void paintComponent(Graphics g) {
@@ -157,27 +114,41 @@ public class Screen extends JPanel implements KeyListener, ActionListener {
 		drawChar(g);
 		g.setColor(Color.BLACK);
 		g.drawRect(charRect.x, charRect.y, charRect.width, charRect.height);
-		g.drawRect(belowRect.x, belowRect.y, belowRect.width, belowRect.height);
-		if (leftRect != null) {
+		g.setColor(Color.RED);
+		g.drawLine(charX, charY + (BLOCKSIZE * 2), belowX(), belowY());
+		g.drawRect(belowX(), belowY(), BLOCKSIZE, BLOCKSIZE);
+		/*if (belowRect != null) {
+			g.drawRect(belowRect.x, belowRect.y, belowRect.width, belowRect.height);
+		}*/
+		/*if (leftRect != null) {
 			g.drawRect(leftRect.x, leftRect.y, leftRect.width, leftRect.height);
 		}
 		if (rightRect != null) {
 			g.drawRect(rightRect.x, rightRect.y, rightRect.width, rightRect.height);
-		}
+		}*/
 	}
 
 	private void drawWorld(Graphics g, BlockEnum[][] world) {
-		for (int r = 0; r < world.length;r++) {
+		for (int r = 0; r < world.length; r++) {
 			for (int c = 0; c < world[0].length; c++) {
 				int blockX = xPos + c * BLOCKSIZE;
 				int blockY = yPos - r * BLOCKSIZE;
 				if (blockX > -BLOCKSIZE && blockX < frame.getWidth() + BLOCKSIZE
 						&& blockY > -BLOCKSIZE && blockY < frame.getHeight() + BLOCKSIZE) {
-				drawBlock(g, world[r][c], blockX, blockY);
+					drawBlock(g, world[r][c], blockX, blockY);
 				}
 			}
 		}
 	}
+
+	private int xToCol(int xPixel) {
+		return Math.round((float)(xPixel - xPos) / (float)(BLOCKSIZE));
+	}
+
+	private int yToRow(int yPixel) {
+		return Math.round((float)(yPos - yPixel) / (float)(BLOCKSIZE));
+	}
+
 
 	private void drawChar(Graphics g) {
 		g.drawImage(character.getImage(), charX, charY, BLOCKSIZE, BLOCKSIZE * 2, this);
@@ -185,8 +156,6 @@ public class Screen extends JPanel implements KeyListener, ActionListener {
 
 	private void drawBlock(Graphics g, BlockEnum b, int x, int y) {
 		if (b != null) {
-			Graphics2D tempG = (Graphics2D) b.getImage().getGraphics();
-			tempG.scale(0.1, 0.1);
 			g.drawImage(b.getImage(), x, y, BLOCKSIZE, BLOCKSIZE, this);
 		}
 	}
@@ -194,43 +163,70 @@ public class Screen extends JPanel implements KeyListener, ActionListener {
 	@Override
 	public void keyPressed(KeyEvent e) {
 		int keyCode = e.getKeyCode();
-		if (keyCode == 39) {
-			xVel = -MOVEINCREMENT;
-		}
-		//left
-		else if (keyCode == 37) {
-			xVel = MOVEINCREMENT;
-		}
-
-		//up 
-		if (keyCode == 38 && touchBelow()) {
-			yVel = 2 * MOVEINCREMENT;
+		if (!keysPressed.contains(keyCode)) {
+			keysPressed.add(keyCode);
+			System.out.println("adding " + keyCode);
 		}
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		int keyCode = e.getKeyCode();
-		if (keyCode == 39 || keyCode == 37) {
-			xVel = 0;
-		}
+		keysPressed.remove(new Integer(keyCode));
+		System.out.println("removing " + keyCode);
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
 	}
-	
-	
+
 	public void actionPerformed(ActionEvent e) {
+
+		//right
+		if (keysPressed.contains(39)) {
+			int worldWidth = WorldBuilder.BIOMEWIDTH * 5 * BLOCKSIZE;
+			int rightXPos = xPos + worldWidth;
+			int charRightXPos = charX + BLOCKSIZE;
+			if (rightXPos - charRightXPos > MOVEINCREMENT) {
+				xVel = -MOVEINCREMENT;
+			}
+			else {
+				xPos = charRightXPos - worldWidth;
+				xVel = 0;
+			}
+		}
+		//left
+		else if (keysPressed.contains(37)) {
+			if (charX - xPos > MOVEINCREMENT) {
+				xVel = MOVEINCREMENT;
+			}
+			else {
+				xPos = charX;
+				xVel = 0;
+			}
+		}
+		else {
+			xVel = 0;
+		}
+
+		//up 
+		if (keysPressed.contains(38) && collidesBelow()) {
+			yVel = 3 * MOVEINCREMENT;
+		}
+		//change position of world
+		//yPos positive makes character go up relative to world
+		//xPos positive makes character go left relative to world
+
 		xPos += xVel;
 		yPos += yVel;
-		if (!touchBelow()) {
-			yVel--;
+		if (!collidesBelow()) {
+			yVel -= (int)((float)MOVEINCREMENT / 3f);
 		}
 		else {
 			yVel = 0;
-			yPos = (yPos/BLOCKSIZE+1) * BLOCKSIZE -1;
+			yPos += distanceBelow() - 1;
 		}
+		//System.out.println("y velocity: " + yVel);
 		repaint();
 	}
 }
